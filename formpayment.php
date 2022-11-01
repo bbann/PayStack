02981ec21c474d6617cb7f8265ca9c4dd15984ff
bbann <91158294+bbann@users.noreply.github.com>
2022-11-01 15:56:50 +0000

    Update formpayment.php

--- formpayment.php
+++ formpayment.php
@@ -12,6 +12,29 @@
     <button type="submit" onclick="payWithPaystack()"> Pay </button>
   </div>
 </form>
-
+      <script>
+      var paymentForm = document.getElementById('paymentForm');
+    paymentForm.addEventListener('submit', payWithPaystack, false);
+    function payWithPaystack() {
+     var handler = PaystackPop.setup({
+      key: 'YOUR_PUBLIC_KEY', // Replace with your public key
+    email: document.getElementById('email-address').value,
+    amount: document.getElementById('amount').value * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
+    currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
+    ref: 'YOUR_REFERENCE', // Replace with a reference you generated
+    currency: 'GHS',
+    callback: function(response) {
+      //this happens after the payment is completed successfully
+      var reference = response.reference;
+      alert('Payment complete! Reference: ' + reference);
+      // Make an AJAX call to your server with the reference to verify the transaction
+    },
+    onClose: function() {
+      alert('Transaction was not completed, window closed.');
+    },
+  });
+  handler.openIframe();
+}
+</script>
 <script src="https://js.paystack.co/v1/inline.js"></script> 
 ?>
