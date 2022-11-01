c7e8495db62fd32272eb87119337a012d89fe674
bbann <91158294+bbann@users.noreply.github.com>
2022-11-01 16:42:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -13,28 +13,30 @@
   </div>
 </form>
       
-      <script>
-     var paymentForm = document.getElementById('paymentForm');
-    paymentForm.addEventListener('submit', payWithPaystack, false);
-    function payWithPaystack() {
-     var handler = PaystackPop.setup({
-      key: 'pk_test_159d0067f8326b83fa30e0b1ad016b14cb69d85c', // Replace with your public key
-    email: document.getElementById('email-address').value,
-    amount: document.getElementById('amount').value * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
-    currency: 'GHS', // Use GHS for Ghana Cedis or USD for US Dollars
-    ref: 'bene7777', // Replace with a reference you generated
- 
-       
-    callback: function(response) {
-      //this happens after the payment is completed successfully
-      var reference = response.reference;
-      alert('Payment complete! Reference: ' + reference);
-      // Make an AJAX call to your server with the reference to verify the transaction
+     <script>
+    const paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+function payWithPaystack(e) {
+  e.preventDefault();
+
+  let handler = PaystackPop.setup({
+    key: 'pk_test_159d0067f8326b83fa30e0b1ad016b14cb69d85c', // Replace with your public key
+    email: document.getElementById("email-address").value,
+    amount: document.getElementById("amount").value * 100,
+    currency:'GHS',
+    ref: 'beebee'+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      window.location = "https://quizpaystack.herokuapp.com/index.php?transaction+call";
+      alert('Transaction terminated.');
     },
-    onClose: function() {
-      alert('Transaction was not completed, window closed.');
-    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+      window.location = "https://quizpaystack.herokuapp.com/success.php?reference=" + response.reference;
+    }
   });
+
   handler.openIframe();
 }
 </script>
