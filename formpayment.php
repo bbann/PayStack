cd28b620c75f7ecab3044aec5ffaa4d3e24eb1c6
bbann <91158294+bbann@users.noreply.github.com>
2022-11-01 15:52:47 +0000

    Update formpayment.php

--- formpayment.php
+++ formpayment.php
@@ -8,14 +8,6 @@
     <label for="amount">Amount</label>
     <input type="tel" id="amount" required />
   </div>
-  <div class="form-group">
-    <label for="first-name">First Name</label>
-    <input type="text" id="first-name" />
-  </div>
-  <div class="form-group">
-    <label for="last-name">Last Name</label>
-    <input type="text" id="last-name" />
-  </div>
   <div class="form-submit">
     <button type="submit" onclick="payWithPaystack()"> Pay </button>
   </div>
